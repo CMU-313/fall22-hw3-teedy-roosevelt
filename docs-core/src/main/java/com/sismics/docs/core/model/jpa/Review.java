bd16a162dff5da435d6ebf42311882135bfb30f3
Jesse Ding <jding@jesses-air.wifi.local.cmu.edu>
2022-09-27 15:35:29 -0400

    adding first review changes

--- docs-core/src/main/java/com/sismics/docs/core/model/jpa/Review.java
+++ docs-core/src/main/java/com/sismics/docs/core/model/jpa/Review.java
@@ -10,26 +10,28 @@ import javax.persistence.Table;
 import com.google.common.base.MoreObjects;
 
 /**
- * Link between an user and a group.
+ * Review entity
+ * Includes the
+ * - User Name
+ * - Rating
+ * - Review ID
+ * - User ID
+ * - Document ID
+ * - Reviewer ID
+ * - Creation Date
+ * - Deletion Date
  * 
  * @author jessedin
  */
 @Entity
 @Table(name = "T_REVIEW")
-public class Review {
+public class Review implements Loggable{
 
     /**
-     * User ID.
+     * User Name.
      */
-    @Id
-    @Column(name = "REV_IDUSER_C", length = 36)
-    private String userId;
-    
-    /**
-     * Document ID.
-     */
-    @Column(name = "REV_IDDOC_C", length = 36)
-    private String docId;
+    @Column(name = "REV_USERNAME_C", length = 36)
+    private String userName;
 
     /**
      * Rating.
@@ -38,24 +40,59 @@ public class Review {
     private Integer rating;
 
     /**
+     * Review ID.
+     */
+    @Id
+    @Column(name = "REV_REVIEWID_C", length = 36)
+    private String reviewId;
+
+    /**
+     * User ID.
+     */
+    @Column(name = "REV_USERID_C", length = 36)
+    private String userId;
+
+    /**
+     * Document ID.
+     */
+    @Column(name = "REV_DOCID_C", length = 36)
+    private String docId;
+
+    /**
      * Reviewer ID.
      */
-    @Column(name = "REV_IDREVIEWER_C", nullable = false, length = 36)
+    @Column(name = "REV_REVIEWERID_C", nullable = false, length = 36)
     private String reviewerId;
 
     /**
-     * Creation date.
+     * Creation date
      */
     @Column(name = "REV_CREATEDATE_D", nullable = false)
     private Date createDate;
-    
-    public String getDocId() {
-        return docId;
+   
+    /**
+     * Deletion date
+     */
+    @Column(name = "REV_DELETEDATE_D", nullable = false)
+    private Date deleteDate;
+
+
+    public String getUserName() {
+        return userName;
     }
 
-    public void setDocId(String id) {
-        this.docId = id;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
 
     public String getUserId() {
         return userId;
@@ -65,20 +102,28 @@ public class Review {
         this.userId = userId;
     }
 
+    public String getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(String reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getDocId() {
+        return docId;
+    }
+
+    public void setDocId(String docId) {
+        this.docId = docId;
+    }
+
     public String getReviewerId() {
         return reviewerId;
     }
 
-    public void setReviewerId(String id) {
-        this.reviewerId = id;
-    }
-
-    public Integer getRating() {
-        return rating;
-    }
-
-    public void setReviewerId(Integer rat) {
-        this.rating = rat;
+    public void setReviewerId(String reviewerid) {
+        this.reviewerId = reviewerid;
     }
 
     public Date getCreateDate() {
@@ -89,14 +134,29 @@ public class Review {
         this.createDate = createDate;
     }
 
+    public Date getDeleteDate() {
+        return deleteDate;
+    }
+
+    public void setDeleteDate(Date deleteDate) {
+        this.deleteDate = deleteDate;
+    }
+
+    @Override
+    public String toMessage() {
+        return reviewId;
+    }
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
+                .add("userName", userName)
+                .add("rating", rating)
                 .add("userId", userId)
+                .add("reviewId", reviewId)
                 .add("docId", docId)
                 .add("reviewerId", reviewerId)
-                .add("rating", rating)
                 .add("createDate", createDate)
+                .add("deleteDate", deleteDate)
                 .toString();
     }
 }
