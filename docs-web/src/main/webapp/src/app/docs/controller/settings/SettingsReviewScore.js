0b4f7e94f8d07c23645a874aae773572a2453399
Nelly Hadlaw <nellyhadlaw@Nellys-MacBook-Pro.local>
2022-10-01 14:33:54 -0400

    added review 1 changes

--- docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsReviewScore.js
+++ docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsReviewScore.js
@@ -6,8 +6,6 @@
 angular.module('docs').controller('SettingsReviewScore', function($scope, Restangular) {
   Restangular.one('app').get().then(function(data) {
     $scope.app = data;
-    console.log('scope is', $scope)
+    console.log('scope is', $scope);
   });
-
-
 });
