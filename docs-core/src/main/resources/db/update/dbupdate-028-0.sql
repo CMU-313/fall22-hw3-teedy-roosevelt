2b3ac838f0732bb56f3530163276c814a1ccbe60
Alejandro Ruiz <aruiz2@andrew.cmu.edu>
2022-10-03 15:11:36 -0400

    created ReviewResource.java and TestReviewResource.java, as well as add entry for dbupdate-028-0.sql

--- docs-core/src/main/resources/db/update/dbupdate-028-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-028-0.sql
@@ -1,3 +1,4 @@
 create memory table T_REVIEW ( REV_USERNAME_C varchar(36), REV_RATING_C integer, REV_REVIEWID_C varchar(36) not null, REV_USERID_C varchar(36), REV_DOCID_C varchar(36), REV_REVIEWERID_C varchar(36), REV_CREATEDATE_D date not null, REV_DELETEDATE_D date, primary key (REV_REVIEWID_C) );
 
 insert into T_REVIEW(REV_USERNAME_C, REV_RATING_C, REV_REVIEWID_C, REV_USERID_C, REV_DOCID_C, REV_REVIEWERID_C, REV_CREATEDATE_D, REV_DELETEDATE_D) values('John Doe', 5, 'review123', 'jdoe', 'doc123', 'reviewerperson', NOW(), NOW());
+insert into T_REVIEW(REV_USERNAME_C, REV_RATI`NG_C, REV_REVIEWID_C, REV_USERID_C, REV_DOCID_C, REV_REVIEWERID_C, REV_CREATEDATE_D, REV_DELETEDATE_D) values('Florentino Perez', 10, 'review1234', 'fperez', 'doc1234', 'reviewerperson', NOW(), NOW());
---
+++ docs-web/src/test/java/com/sismics/docs/rest/TestReviewResource.java
@@ -0,0 +1,51 @@
+package com.sismics.docs.rest;
+
+import com.sismics.util.filter.TokenBasedSecurityFilter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+import javax.json.JsonWriter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Tests the ReviewResource.java file
+ *
+ * @author aruiz2
+ */
+
+public class TestReviewResource extends BaseJerseyTest {
+    private static final Logger log = LoggerFactory.getLogger(TestReviewResource.class);
+
+    /**
+     * Tests ReviewResource.java
+     */
+    @Test
+    public void testReviewResource() {
+        // Login admin
+        String adminToken = clientUtil.login("admin", "admin", false);
+        String username = "username";
+        String docId = "docId";
+        String averageRating = "avgRating";
+        JsonObject json = target().path("/user_rating").request()
+                .cookie(TokenBasedSecurityFilter.COOKIE_NAME, adminToken)
+                .get(JsonObject.class);
+
+        // Assert correct data from 'dbupdate-028-0.sql
+        Assert.assertEquals(2, json.getJsonArray("ratings").size());
+        JsonObject entry1 = json.getJsonArray("ratings").getJsonObject(1);
+        Assert.assertEquals("John Doe", entry1.getString(username));
+        Assert.assertEquals("doc123", entry1.getString(docId));
+        Assert.assertEquals(5, entry1.getInt(averageRating));
+
+        JsonObject entry2 = json.getJsonArray("ratings").getJsonObject(0);
+        Assert.assertEquals("Florentino Perez", entry2.getString(username));
+        Assert.assertEquals("doc1234", entry2.getString(docId));
+        Assert.assertEquals(10, entry2.getInt(averageRating));
+        log.info(json.toString());
+        log.info("ALEJANDRO'S TEST DID NOT ERROR!\n\n");
+    }
+}
