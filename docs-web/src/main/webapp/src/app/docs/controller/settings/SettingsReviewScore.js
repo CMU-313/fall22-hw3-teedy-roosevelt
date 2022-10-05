a02aea760db3b02ffbf98c538868ea4623196442
SiddharthParatkar <siddharthparatkar@yahoo.com>
2022-10-04 22:04:39 -0400

    Displaying chart with dummy data

--- docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsReviewScore.js
+++ docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsReviewScore.js
@@ -8,4 +8,25 @@ angular.module('docs').controller('SettingsReviewScore', function($scope, Restan
     $scope.app = data;
     console.log('scope is', $scope);
   });
+
+  $scope.drawGraph = function() {
+    var riceData = {
+      labels : ["January","February","March","April","May","June"],
+      datasets : [
+          {
+              fillColor : "rgba(172,194,132,0.4)",
+              strokeColor : "#ACC26D",
+              pointColor : "#fff",
+              pointStrokeColor : "#9DB86D",
+              data : [203000,15600,99000,25100,30500,24700]
+          }
+      ]
+    }
+    
+    var rice = document.getElementById('rice').getContext('2d');
+    new Chart(rice, {
+        type: "line",
+        data: riceData, 
+    });
+  }
 });
