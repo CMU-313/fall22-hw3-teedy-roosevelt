da2625ae91b595d8b15d0f22626afce86171da91
Siddharth Paratkar <siddharthparatkar@yahoo.com>
2022-10-05 18:11:37 -0400

    Avg review chart (#16)
    * Displaying chart with dummy data
    
    * made all api connections such that chart has been displayed with the correct data
    
    * making bar chart horizontal

--- docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsReviewScore.js
+++ docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsReviewScore.js
@@ -4,8 +4,48 @@
  * Settings monitoring controller.
  */
 angular.module('docs').controller('SettingsReviewScore', function($scope, Restangular) {
-  Restangular.one('app').get().then(function(data) {
-    $scope.app = data;
+  Restangular.one('user_rating').get().then(function(data) {
+    $scope.user_rating = data;
     console.log('scope is', $scope);
   });
+
+  $scope.drawGraph = function() {
+
+    var data = $scope.user_rating.ratings;
+    console.log($scope.user_rating.ratings);
+
+    var labels = [];
+    var sets = [];
+    angular.forEach(data, function(data){
+      labels.push(data.username + "/" + data.docId);
+      sets.push(data.avgRating);
+    });
+    console.log(labels);
+    console.log(sets);
+    
+    var barData = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Average rating for user/docId",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: sets
+        }
+      ]
+    };
+    
+    var countries = document.getElementById('bar-chart').getContext('2d');
+    new Chart(countries, {
+        type: "bar",
+        data: barData, 
+        options: {
+          legend: { display: false },
+          title: {
+            display: true,
+            text: 'Predicted world population (millions) in 2050'
+          },
+          indexAxis: 'y',
+        }
+    });
+  }
 });
