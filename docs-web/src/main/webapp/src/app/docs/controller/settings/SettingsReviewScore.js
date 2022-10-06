1c9ea7554d0494d3eaa52792c85b80215f3726bc
Qingyang-Sophia <94241974+Qingyang-Sophia@users.noreply.github.com>
2022-10-06 19:05:13 -0400

    Front end Search Bar  (#17)
    * added search bar to Average Review Scores
    
    * added scope for searched user
    
    * fixed unique chart issue by destroying previous instances of chart
    
    * changed search bar to display appropriate graph results
    
    * changed display text
    
    * Make the search button call drawGraph() when clicked on. Changed search input box placeholder message
    
    Co-authored-by: Nelly Hadlaw <nellyhadlaw@Nellys-MacBook-Pro.local>
    Co-authored-by: Qingyang Cao <qingyang@Qingyangs-MacBook-Air-2.local>

--- docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsReviewScore.js
+++ docs-web/src/main/webapp/src/app/docs/controller/settings/SettingsReviewScore.js
@@ -4,15 +4,50 @@
  * Settings monitoring controller.
  */
 angular.module('docs').controller('SettingsReviewScore', function($scope, Restangular) {
+  console.log('first scope', $scope)
   Restangular.one('user_rating').get().then(function(data) {
     $scope.user_rating = data;
     console.log('scope is', $scope);
   });
 
-  $scope.drawGraph = function() {
+  let curr_chart = undefined;
 
+  $scope.drawGraph = function() {
+    let search_bar_text = `Displaying all users`;
+    if ($scope.searchUser != undefined && $scope.searchUser != '') {
+      search_bar_text = `Displaying "${$scope.searchUser}"`;
+    }
+    console.log('searchUser is', $scope.searchUser);
+
+    //need to destroy previous instances to curr chart
+    //so that charts can use unique IDs
+    if (curr_chart != undefined){
+      curr_chart.destroy();
+    }
+
+    document.getElementById("search-bar-text").innerHTML = search_bar_text;
     var data = $scope.user_rating.ratings;
-    console.log($scope.user_rating.ratings);
+
+    console.log('data is', data);
+    //now we want to manipulate data to correspond with the search results
+    var new_data = [];
+    let curr_search = $scope.searchUser;
+    if (curr_search != undefined) {
+      let curr_search = $scope.searchUser.toLowerCase();
+      for (let i = 0; i < data.length; i++) {
+          let curr_username = data[i].username.toLowerCase();
+          if (curr_username.includes(curr_search)) {
+            new_data.push(data[i]);
+          }
+      }
+      if (new_data.length != 0) {
+        data = new_data;
+      }else {
+        document.getElementById("search-bar-text").innerHTML = `No users for "${curr_search}", displaying all users`;
+      }
+
+    }
+
 
     var labels = [];
     var sets = [];
@@ -34,15 +69,15 @@ angular.module('docs').controller('SettingsReviewScore', function($scope, Restan
       ]
     };
     
-    var countries = document.getElementById('bar-chart').getContext('2d');
-    new Chart(countries, {
+
+    var user_data = document.getElementById('bar-chart').getContext('2d');
+    curr_chart = new Chart(user_data, {
         type: "bar",
         data: barData, 
         options: {
           legend: { display: false },
           title: {
             display: true,
-            text: 'Predicted world population (millions) in 2050'
           },
           indexAxis: 'y',
         }
